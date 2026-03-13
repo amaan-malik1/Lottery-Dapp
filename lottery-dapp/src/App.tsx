efd2bc73318a6a79cd14d9bce38bea25caf44833
Amaan Malik <amikm1077@gmail.com>
2026-03-13 11:59:18 +0530

    fix: Ui fix

--- lottery-dapp/src/App.tsx
+++ lottery-dapp/src/App.tsx
@@ -1,6 +1,10 @@
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"
 import { WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+import Navbar from "./components/Navbar";
+
 function App() {
   const RPC = import.meta.env.VITE_RPC_URL;
 
@@ -11,16 +15,17 @@ function App() {
       <ConnectionProvider endpoint={RPC}>
         <WalletProvider wallets={[]} autoConnect>
           <WalletModalProvider>
-            {/* connect and disconnect button */}
-            <div className="flex justify-between items-center ">
-              <div className="flex justify-evenly gap-4 items-center w-full p-4">
+            {/* navbar */}
+            <div className="w-full flex justify-between items-center px-4 py-2 bg-slate-600">
+              <Navbar />
+              {/* connect and disconnect button */}
+              <div className="flex items-center gap-2">
                 <WalletMultiButton />
                 <WalletDisconnectButton />
               </div>
-              {/* other componenets here */}
-              <div></div>
-
             </div>
+            {/* other componenets here */}
+            <div></div>
           </WalletModalProvider>
 
         </WalletProvider>
---
+++ lottery-dapp/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav>
+            {/* logo */}
+            <div>
+                <a href="/"> LootSOL</a>
+            </div>
+            <div>
+                
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
