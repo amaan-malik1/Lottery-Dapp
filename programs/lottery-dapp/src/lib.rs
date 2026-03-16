9698b014f9802d9940d91b6e94e2bd4e51b4778c
amaan-malik1 <amikm1077@gmail.com>
2026-03-16 06:51:49 +0000

    init contract

--- programs/lottery-dapp/src/lib.rs
+++ programs/lottery-dapp/src/lib.rs
@@ -1,4 +1,40 @@
+mod constants;
+use crate::constants::*;
+
 use anchor_lang::{
     prelude::*,
-    
+    solana_program::{program::invoke, system_instruction::transfer},
+};
+
+declare_id!("111111111111111111111111111111111111");
+
+#[program]
+pub mod lottery_program {
+    use super::*;
+
+    pub fn init_master(ctx: Context<InitMaster>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitMaster<'info> {
+    #[account(
+        init,
+        payer  = payer,
+        space = 4 + 8,
+        seeds = [MASTER_SEED.as_bytes()],
+        bump,
+    )]
+    pub master: Account<'info, Master>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Master {
+    pub last_id: u32,
 }
